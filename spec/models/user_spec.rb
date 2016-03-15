340bcd0c590c1ad0908676d55dc27a618f728209
Stanley Hansen <stanley321@gmail.com>
2016-03-14 19:49:21 -0700

    Refactor as per "config.include FactoryGirl::Syntax::Methods" in RSpec Config.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe User, type: :model do
     end
     it "requires a unique email" do
 
-      u = FactoryGirl.create(:user)
+      u = create(:user)
       u1 = User.new email: u.email
       u1.valid?
       expect(u1.errors).to have_key(:email)
@@ -33,17 +33,17 @@ RSpec.describe User, type: :model do
   end
   describe ".full_name" do
     it "concatenates the first name and last name" do
-      u = FactoryGirl.build(:user)
+      u = build(:user)
       expect(u.full_name).to include("#{u.first_name}", "#{u.last_name}")
     end
     it "returns the first name if the last name is missing" do
-      u = FactoryGirl.build(:user, {last_name: nil})
+      u = build(:user, {last_name: nil})
       expect(u.full_name).to eq(u.first_name)
     end
   end
   describe "password generating" do
     it "generates a password digest on creation" do
-      u = FactoryGirl.build(:user)
+      u = build(:user)
       expect(u.password_digest).to be
     end
   end
