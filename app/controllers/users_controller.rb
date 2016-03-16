2735ff39cd99be1ce1353981af17819db1b3900c
Stanley Hansen <stanley321@gmail.com>
2016-03-15 17:07:17 -0700

    Typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
   
   def create
-   @user = User.new user_params
+    @user = User.new user_params
     if @user.save
       sign_in(@user)
       redirect_to root_path, notice: "User Created!"
