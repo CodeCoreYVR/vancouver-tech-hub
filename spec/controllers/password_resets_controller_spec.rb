bc84a594ef4ea5c627e11089c43808cacfd63a6c
Stanley Hansen <stanley321@gmail.com>
2016-03-17 10:14:28 -0700

    Adding Specs to password reset controller

--- spec/controllers/password_resets_controller_spec.rb
+++ spec/controllers/password_resets_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe PasswordResetsController, type: :controller do
-
+  let(:user) {create(:user)}
   describe "GET #new" do
     it "returns http success" do
       get :new
@@ -9,4 +9,19 @@ RSpec.describe PasswordResetsController, type: :controller do
     end
   end
 
+  describe "POST #create" do
+    context 'with existing email' do
+      before do
+        post :create, password_reset: {email: user.email }
+      end
+      it 'redirects use to the root path' do
+        expect(response).to redirect_to(root_path)
+      end
+      it 'sends a reset password email' do
+        expect(UserMailer).to receive(:password_reset).with(user)
+      end
+    end
+    context 'without existing email' do
+    end
+  end
 end
